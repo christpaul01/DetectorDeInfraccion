b2f267c2e8864598632d1f79ecd3bc74405b3c1e
Christopher Paul <45078023+christpaul01@users.noreply.github.com>
2024-07-14 00:49:20 -0400

    Actualizacion Ejemplo POST

--- camaras/views.py
+++ camaras/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 
 def camaras(request):
-    context = {
-        'camera_name': 'Camera 1',
-        'camera_location': 'Main Street'
-    }
-    return render(request, 'configCamaras.html', context)
+    if request.method == 'POST':
+        # Handle form submission
+        camera_name = request.POST.get('camera_name')
+        camera_location = request.POST.get('camera_location')
+        print(camera_name, camera_location)
+        return redirect('camaras')
+    else:
+        context = {
+            'camera_name': 'Camera 1',
+            'camera_location': 'Main Street'
+        }
+        return render(request, 'configCamaras.html', context)
