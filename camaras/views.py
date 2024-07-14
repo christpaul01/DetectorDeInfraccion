7e22ecad150e8f5dd15c6158d1dd44210eebbc7b
Christopher Paul <45078023+christpaul01@users.noreply.github.com>
2024-07-14 00:59:04 -0400

    Modificacion Ejemplo

--- camaras/views.py
+++ camaras/views.py
@@ -7,11 +7,15 @@ def camaras(request):
         # Handle form submission
         camera_name = request.POST.get('camera_name')
         camera_location = request.POST.get('camera_location')
-        print(camera_name, camera_location)
+        if camera_name and camera_location:
+            print(camera_name, camera_location)
+        else:
+            print('Invalid form submission')
+
         return redirect('camaras')
     else:
         context = {
-            'camera_name': 'Camera 1',
-            'camera_location': 'Main Street'
+            # 'camera_name': 'Camera 1',
+            # 'camera_location': 'Main Street'
         }
         return render(request, 'configCamaras.html', context)
