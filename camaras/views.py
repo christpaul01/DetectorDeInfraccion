8b64c6a0e489d4d165ee3089088ddb90e1a93340
Christopher Paul <45078023+christpaul01@users.noreply.github.com>
2024-07-13 23:51:58 -0400

    Subiendo ejemplo de modelos de template

--- camaras/views.py
+++ camaras/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 from django.template import loader
 
 def camaras(request):
-  template = loader.get_template('configCamaras.html')
-  return HttpResponse(template.render())
+    context = {
+        'camera_name': 'Camera 1',
+        'camera_location': 'Main Street'
+    }
+    return render(request, 'configCamaras.html', context)
