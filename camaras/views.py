19db9101288a9c90dc84b38a778d55f20a8cfdba
Christopher Paul <45078023+christpaul01@users.noreply.github.com>
2024-07-28 03:45:22 -0400

    Se agrego template para errores

--- camaras/views.py
+++ camaras/views.py
@@ -2,26 +2,46 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Camara
+from django.db.models import Max
 
+
+def get_next_camera_id():
+    """
+    Returns the next available ID for a new camera.
+    """
+    if Camara.objects.exists():
+        max_id = Camara.objects.aggregate(Max('id_camara'))['id_camara__max']
+        return max_id + 1
+    else:
+        return 1
 
 def home(request):
     camaras = Camara.objects.all()
-    return render(request, "configCamaras.html", {"camaras": camaras})
-
+    next_id = get_next_camera_id()
+    context = {"camaras": camaras, "next_id": next_id}
+    return render(request, "configCamaras.html", context)
 
 
 def registarCamara(request):
-    idCamara = request.POST['idCamara']
+    idCamara = get_next_camera_id()
     nombre = request.POST['nombreCamara']
     estado = request.POST['estadoCamara']
     resolucionCamara = request.POST['resolucionCamara']
 
-    camara = Camara.objects.create(id_camara=idCamara, nombre_camara=nombre, estado_camara=estado, resolucion_camara=resolucionCamara)
+    Camara.objects.create(id_camara=idCamara, nombre_camara=nombre, estado_camara=estado, resolucion_camara=resolucionCamara)
     return redirect('/')
 
 def editarCamara(request,id_camara):
-    camara = Camara.objects.get(id_camara=id_camara)
-    return render(request,'editarCamara.html', {"camara": camara})
+    try:
+        camara = Camara.objects.get(id_camara=id_camara)
+        return render(request, 'editarCamara.html', {"camara": camara})
+    except Camara.DoesNotExist:
+        error_type = "Error de procesamiento"
+        error_message = "No se encontró la cámara solicitada."
+        context = {"error_type": error_type, "error_message": error_message}
+
+        return render(request, 'error.html', context)
+
 
 #Error para completar la edicion, se supone que hay que enviar el id_camara pero en el tutorial no lo hacen de esa forma
 def edicionCamara(request):
